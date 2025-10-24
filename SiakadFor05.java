dd81e9b1af194078b6af78afc6815b2c60c6b28f
Aliyah Miranti Daleya <254107060060@student.polinema.ac.id>
2025-10-24 10:59:34 +0700

    Modifikasi Percobaan 1

--- SiakadFor05.java
+++ SiakadFor05.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+
 public class SiakadFor05 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double nilai, tertinggi = 0, terendah = 100; 
+        double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0; // Variabel untuk menghitung jumlah lulus dan tidak lulus
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -15,10 +17,19 @@ public class SiakadFor05 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            // Hitung jumlah mahasiswa yang lulus dan tidak lulus
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
 
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa yang lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + tidakLulus);
 
         sc.close();
     }
