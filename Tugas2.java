9a24afdb4a7af4741740ad4b4f0375547b10db86
Aliyah Miranti Daleya <254107060060@student.polinema.ac.id>
2025-10-24 10:57:32 +0700

    Tugas 2

---
+++ Tugas2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Parkir {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int jenis, durasi;
+        int total = 0;
+
+        while (true) {
+            System.out.println("Masukkan jenis kendaraan (1 Mobil, 2 Motor, 0 Keluar):");
+            jenis = input.nextInt();
+
+            if (jenis == 0) {
+                break;
+            }
+
+            if (jenis == 1 || jenis == 2) {
+                System.out.println("Masukkan durasi parkir (jam):");
+                durasi = input.nextInt();
+
+                if (durasi > 5) {
+                    total += 12500;
+                } else {
+                    if (jenis == 1) {
+                        total += durasi * 3000;
+                    } else if (jenis == 2) {
+                        total += durasi * 2000;
+                    }
+                }
+            }
+        }
+
+        System.out.println("Total pembayaran parkir: Rp " + total);
+        input.close();
+    }
+}
